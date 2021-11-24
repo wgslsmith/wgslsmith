1515e60172db675321107cbcf87cfdfd248739d7
Hasan Ali <hasan@hasali.dev>
2021-11-24 12:37:28 +0000

    Add option to print ast for debugging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
+use clap::Parser;
 use wgslsmith::generator::Generator;
 use wgslsmith::types::TypeConstraints;
 
+#[derive(Parser)]
+struct Options {
+    #[clap(short, long, about = "Print ast instead of WGSL code")]
+    debug: bool,
+}
+
 fn main() {
-    println!("{}", Generator::new().gen_expr(TypeConstraints::INT));
+    let options = Options::parse();
+    let expr = Generator::new().gen_expr(TypeConstraints::INT);
+
+    if options.debug {
+        println!("{:#?}", expr);
+    } else {
+        println!("{}", expr);
+    }
 }
