4a2371a79736c7594cd650563c57b8405fc65510
Hasan Ali <hasan@hasali.dev>
2021-12-29 23:51:54 +0000

    Implement grammar for const literals

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,8 +1,34 @@
+#[derive(pest_derive::Parser)]
+#[grammar = "grammar.pest"]
+struct WGSLParser;
+
 #[cfg(test)]
 mod tests {
+    use pest::Parser;
+
+    use crate::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn bool_literal() {
+        let result = WGSLParser::parse(Rule::const_literal, "true").unwrap();
+        assert_eq!(result.as_str(), "true");
+
+        let result = WGSLParser::parse(Rule::const_literal, "false").unwrap();
+        assert_eq!(result.as_str(), "false");
+    }
+
+    #[test]
+    fn int_literal() {
+        let result = WGSLParser::parse(Rule::int_literal, "123").unwrap();
+        assert_eq!(result.as_str(), "123");
+
+        let result = WGSLParser::parse(Rule::int_literal, "-123").unwrap();
+        assert_eq!(result.as_str(), "-123");
+    }
+
+    #[test]
+    fn uint_literal() {
+        let result = WGSLParser::parse(Rule::uint_literal, "123u").unwrap();
+        assert_eq!(result.as_str(), "123u");
     }
 }
