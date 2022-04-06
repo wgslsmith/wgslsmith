33b79bb0d6e7c162da62a73cb2b174c6961cf1dc
Hasan Ali <hasan@hasali.dev>
2022-04-06 21:21:46 +0100

    Accept path to shader as argument

--- crates/harness/src/main.rs
+++ crates/harness/src/main.rs
@@ -1,16 +1,27 @@
 mod dawn;
 mod wgpu;
 
+use std::fs::File;
 use std::io::Read;
 
+use clap::Parser;
 use color_eyre::Result;
 use common::ShaderMetadata;
 
+#[derive(Parser)]
+struct Options {
+    /// Path to wgsl shader program to be executed (use '-' for stdin)
+    #[clap(default_value_t = String::from("-"))]
+    input: String,
+}
+
 fn main() -> Result<()> {
+    let options = Options::parse();
+
     color_eyre::install()?;
     env_logger::init();
 
-    let input = read_shader_from_stdin()?;
+    let input = read_shader_from_path(&options.input)?;
     let (meta, shader) = input.split_once('\n').unwrap();
     let meta: ShaderMetadata = meta
         .strip_prefix("//")
@@ -37,8 +48,14 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn read_shader_from_stdin() -> Result<String> {
+fn read_shader_from_path(path: &str) -> Result<String> {
+    let mut input: Box<dyn Read> = match path {
+        "-" => Box::new(std::io::stdin()),
+        path => Box::new(File::open(path)?),
+    };
+
     let mut shader = String::new();
-    std::io::stdin().read_to_string(&mut shader)?;
+    input.read_to_string(&mut shader)?;
+
     Ok(shader)
 }
