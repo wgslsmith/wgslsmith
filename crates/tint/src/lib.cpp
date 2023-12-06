79bda017db3e79f9af4e05870e28b735354778f4
agg22 <amber.gorzynski22@imperial.ac.uk>
2023-12-06 11:34:39 +0000

    Update namespaces

--- crates/tint/src/lib.cpp
+++ crates/tint/src/lib.cpp
@@ -5,26 +5,26 @@
 
 bool validate_shader(const char* source) {
     auto source_file = std::make_unique<tint::Source::File>("[memory]", source);
-    auto program = std::make_unique<tint::Program>(tint::reader::wgsl::Parse(source_file.get()));
+    auto program = std::make_unique<tint::Program>(tint::wgsl::reader::Parse(source_file.get()));
     return program->IsValid();
 }
 
 std::unique_ptr<std::string> compile_shader_to_hlsl(const char* source) {
     auto source_file = std::make_unique<tint::Source::File>("[memory]", source);
-    auto program = std::make_unique<tint::Program>(tint::reader::wgsl::Parse(source_file.get()));
+    auto program = std::make_unique<tint::Program>(tint::wgsl::reader::Parse(source_file.get()));
 
     if (!program->IsValid()) {
         return nullptr;
     }
 
-    tint::transform::Manager transform_manager;
-    tint::transform::DataMap transform_inputs;
+    tint::ast::transform::Manager transform_manager;
+    tint::ast::transform::DataMap transform_inputs;
 
-    transform_inputs.Add<tint::transform::Renamer::Config>(
-        tint::transform::Renamer::Target::kHlslKeywords,
+    transform_inputs.Add<tint::ast::transform::Renamer::Config>(
+        tint::ast::transform::Renamer::Target::kHlslKeywords,
         /* preserve_unicode */ false
     );
-    transform_manager.Add<tint::transform::Renamer>();
+    transform_manager.Add<tint::ast::transform::Renamer>();
 
     auto transformed = transform_manager.Run(program.get(), std::move(transform_inputs));
     if (!transformed.program.IsValid()) {
@@ -33,8 +33,8 @@ std::unique_ptr<std::string> compile_shader_to_hlsl(const char* source) {
 
     *program = std::move(transformed.program);
 
-    tint::writer::hlsl::Options gen_options = {};
-    auto result = tint::writer::hlsl::Generate(program.get(), gen_options);
+    tint::hlsl::writer::Options gen_options = {};
+    auto result = tint::hlsl::writer::Generate(program.get(), gen_options);
     if (!result.success) {
         return nullptr;
     }
@@ -44,20 +44,20 @@ std::unique_ptr<std::string> compile_shader_to_hlsl(const char* source) {
 
 std::unique_ptr<std::string> compile_shader_to_msl(const char* source) {
     auto source_file = std::make_unique<tint::Source::File>("[memory]", source);
-    auto program = std::make_unique<tint::Program>(tint::reader::wgsl::Parse(source_file.get()));
+    auto program = std::make_unique<tint::Program>(tint::wgsl::reader::Parse(source_file.get()));
 
     if (!program->IsValid()) {
         return nullptr;
     }
 
-    tint::transform::Manager transform_manager;
-    tint::transform::DataMap transform_inputs;
+    tint::ast::transform::Manager transform_manager;
+    tint::ast::transform::DataMap transform_inputs;
 
-    transform_inputs.Add<tint::transform::Renamer::Config>(
-        tint::transform::Renamer::Target::kMslKeywords,
+    transform_inputs.Add<tint::ast::transform::Renamer::Config>(
+        tint::ast::transform::Renamer::Target::kMslKeywords,
         /* preserve_unicode */ false
     );
-    transform_manager.Add<tint::transform::Renamer>();
+    transform_manager.Add<tint::ast::transform::Renamer>();
 
     auto transformed = transform_manager.Run(program.get(), std::move(transform_inputs));
     if (!transformed.program.IsValid()) {
