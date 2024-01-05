5d2fb917c881481bc80f8e5b40d577626fe5883e
agg22 <amber.gorzynski22@imperial.ac.uk>
2024-01-05 10:39:33 +0000

    Remove mut from shader since current options no longer mutate shader

--- crates/preprocessor/src/lib.rs
+++ crates/preprocessor/src/lib.rs
@@ -1,6 +1,6 @@
 pub use ast::writer::Options;
 
-pub fn preprocess(options: Options, mut shader: String) -> String {
+pub fn preprocess(options: Options, shader: String) -> String {
     if options.module_scope_constants {
         panic!("module scope constants are not supported yet");
     }
