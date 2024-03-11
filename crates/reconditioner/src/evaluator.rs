73e548ce6b125fc97a1e306861f11546e5514463
agg22 <amber.gorzynski22@imperial.ac.uk>
2024-03-11 09:02:20 +0000

    Add ConNode struct to use in concretization reconditioning pass

--- crates/reconditioner/src/evaluator.rs
+++ crates/reconditioner/src/evaluator.rs
@@ -1,5 +1,43 @@
 use ast::*;
 
+/* Concretized node struct contains a concretized node and 
+   the value from evaluating that node. The value is None
+   if the node cannot be evaluated at shader creation time
+   (i.e. the node is not a const expression, for example if
+   it contains a runtime variable).
+*/
+pub enum Value {
+    Lit(Lit),
+    TypeCons(TypeConsExpr),
+}
+
+pub struct ConNode {
+    node : ExprNode,
+    value : Option<Value>,
+}
+
+
+// from ExprNode to ConNode (for passing into concretize fns)
+impl From<ExprNode> for ConNode {
+    fn from(node : ExprNode) -> Self {
+        ConNode {
+            node : node,
+            value : None,
+        }
+     }
+}
+
+// from ConNode to ExprNode (for extracting expr to rebuild program, when we don't care about values)
+impl From<ConNode> for ExprNode {
+    fn from(con : ConNode) -> Self {
+        ExprNode {
+            data_type : con.node.data_type,
+            expr : con.node.expr,
+        }
+    }
+}
+
+
 #[derive(Default)]
 pub struct Options {
     pub placeholder: bool,
@@ -43,6 +81,7 @@ impl Evaluator {
     }
 
     fn concretize_fn(&self, mut decl: FnDecl) -> FnDecl {
+
         decl.body = decl
             .body
             .into_iter()
@@ -67,13 +106,13 @@ impl Evaluator {
             }) => VarDeclStatement::new(
                 ident,
                 data_type,
-                initializer.map(|e| self.concretize_expr(e)),
+                initializer.map(|e| self.concretize_expr(e).into()),
             ).into(),
             Statement::Assignment(AssignmentStatement {lhs, op, rhs}) => {
                 AssignmentStatement::new(
                     lhs,
                     op,
-                    self.concretize_expr(rhs),
+                    self.concretize_expr(rhs.into()),
                 ).into()
             },
             Statement::Compound(s) => {
@@ -84,20 +123,20 @@ impl Evaluator {
                 body,
                 else_,
             }) => IfStatement::new(
-                    self.concretize_expr(condition),
+                    self.concretize_expr(condition.into()),
                     body.into_iter().map(|s| self.concretize_stmt(s)).collect(),
                     )
                 .with_else(else_.map(|els| *els))
                 .into(),
             Statement::Return(ReturnStatement {value}) => ReturnStatement {
-                value: value.map(|e| self.concretize_expr(e)),
+                value: value.map(|e| self.concretize_expr(e).into()),
             }.into(),
             Statement::Switch(SwitchStatement {
                 selector,
                 cases,
                 default,
             }) => SwitchStatement::new(
-                self.concretize_expr(selector),
+                self.concretize_expr(selector).into(),
                 cases
                     .into_iter()
                     .map(|c| self.concretize_switch_case(c))
@@ -111,7 +150,7 @@ impl Evaluator {
                 ident,
                 args
                     .into_iter()
-                    .map(|e| self.concretize_expr(e))
+                    .map(|e| self.concretize_expr(e).into())
                     .collect()
                 ).into(),
             Statement::Loop(LoopStatement {body}) => LoopStatement::new(
@@ -119,7 +158,7 @@ impl Evaluator {
             Statement::ForLoop(ForLoopStatement {header, body}) => ForLoopStatement::new(
                 ForLoopHeader {
                     init : header.init.map(|init| self.concretize_for_init(init)),
-                    condition: header.condition.map(|e| self.concretize_expr(e)),
+                    condition: header.condition.map(|e| self.concretize_expr(e).into()),
                     update : header.update.map(|update| self.concretize_for_update(update)),
                     },
                 body.into_iter().map(|s| self.concretize_stmt(s)).collect(),
@@ -139,7 +178,7 @@ impl Evaluator {
            }) => ForLoopInit::VarDecl(VarDeclStatement::new(
                ident,
                data_type,
-               initializer.map(|e| self.concretize_expr(e)),
+               initializer.map(|e| self.concretize_expr(e).into()),
                )),
        }
    }
@@ -172,7 +211,7 @@ impl Evaluator {
             LhsExpr::Postfix(expr, postfix) => LhsExpr::Postfix(
                 self.concretize_lhs_expr(*expr).into(),
                 match postfix {
-                    Postfix::Index(index) => Postfix::Index(self.concretize_expr(*index).into()),
+                    Postfix::Index(index) => Postfix::Index(Box::new(self.concretize_expr(*index).into())),
                     Postfix::Member(string) => Postfix::Member(string),
                 }),
             LhsExpr::Deref(_) => todo!(),
@@ -196,47 +235,161 @@ impl Evaluator {
            .collect();
 
        SwitchCase {
-           selector : concretized_selector,
+           selector : concretized_selector.into(),
            body : concretized_body,
        }
    }
 
-    fn concretize_expr(&self, node: ExprNode) -> ExprNode {
+    fn concretize_expr(&self, node: ExprNode) -> ConNode {
 
         //TODO: if expr contains var, return (since not concretizable)
 
-        let concretized = match node.expr {
-            Expr::Lit(lit) => self.test_lit(lit).into(), // placeholder
+        match node.expr {
+            Expr::Lit(lit) => self.test_lit(lit), // placeholder
+            
+            /* TypeCons is a (e.g. vector) type constructor
+               it should be set up as a ConNode where the 
+               value is a vector of the correct type, featuring
+               evaluated (optional) values
             Expr::TypeCons(expr) => Expr::TypeCons(TypeConsExpr::new(
                 expr.data_type,
                 expr.args
                     .into_iter()
-                    .map(|e| self.concretize_expr(e))
+                    .map(|e| self.concretize_expr(e).into())
                     .collect()
-            )),
-            Expr::Var(expr) => todo!(),
-            Expr::UnOp(expr) => todo!(),
-            Expr::BinOp(expr) => todo!(),
-            Expr::FnCall(expr) => todo!(),
-            Expr::Postfix(expr) => todo!(),
-        };
+            )).into(),
+            */
+            Expr::TypeCons(expr) => ConNode {node : expr.into(), value : None},//TODO
+            Expr::UnOp(expr) => {
 
-        ExprNode {
-            data_type: node.data_type,
-            expr : concretized,
-        }
+                let con_inner = self.concretize_expr(*expr.inner);
+
+                return self.concretize_unop(node.data_type, expr.op, con_inner);
+            }
+            Expr::BinOp(expr) => {
+                let left = self.concretize_expr(*expr.left);
+                let right = self.concretize_expr(*expr.right);
+
+                return self.concretize_bin_op(node.data_type, expr.op, left, right);
+            }
+            Expr::FnCall(expr) => ConNode { 
+                node : ExprNode { data_type : node.data_type, expr : expr.into()}, 
+                value : None}, //TODO
+            Expr::Postfix(expr) =>  ConNode { 
+                node : ExprNode { data_type : node.data_type, expr : expr.into()}, 
+                value : None}, //TODO
+            Expr::Var(expr) => ConNode {
+                node : ExprNode { data_type : node.data_type, expr : expr.into()},
+                value : None}, //TODO
+        }   
+
+        
+        
     }
 
-    fn test_lit(&self, lit : Lit) -> Expr {
+    fn test_lit(&self, lit : Lit) -> ConNode {
        
         //TODO: placeholder to test operation of concretization 
-        let val = match lit {
+        let value = match lit {
             Lit::I32(_) => Lit::I32(1),
             Lit::U32(_) => Lit::U32(1),
             Lit::F32(_) => Lit::F32(1.0),
-            e => e,
+            Lit::Bool(b) => Lit::Bool(b),
         };
 
-        return Expr::Lit(val)
+        ConNode {
+            node : ExprNode {
+                data_type : lit.data_type(),
+                expr : Expr::Lit(value),
+            },
+            value : Some(Value::Lit(value)),
+        }
     }
+
+    fn concretize_bin_op(
+        &self, 
+        data_type : DataType, 
+        op : BinOp, 
+        left : ConNode, 
+        right : ConNode
+    ) -> ConNode {
+
+        // we have already concretized left and right
+        // so now just need to evaluate left op right and 
+        // determine whether this needs to be replaced
+
+        /*
+        let value = match op {
+            //BinOp::Plus => self.concretize_plus(data_type, left, right),
+            BinOp::Plus => todo!(),
+            BinOp::Minus => todo!(),
+            e => todo!(),
+        };
+        */
+    
+        // check if resulting value is within bounds
+        // if not, then replace value and expression
+        // with some suitable default binop (e.g. 1 + 1)
+        // replacement must be of appropriate type
+        let concrete_left = left;
+        let concrete_right = right;
+        //let concrete_value = value;
+
+        ConNode {
+            node : ExprNode {
+                data_type: data_type,
+                expr: Expr::BinOp(BinOpExpr::new(op, concrete_left, concrete_right))
+            },
+            value : None,
+            //value : Some(Value::Lit(concrete_value)),
+        }
+
+    }
+
+
+    fn concretize_unop(
+        &self,
+        data_type : DataType,
+        op : UnOp,
+        inner : ConNode
+    ) -> ConNode {
+
+        // evaluate unop
+        let value = None;
+        /*
+        let value = match op {
+            UnOp::Neg => todo!(),
+            UnOp::Not => todo!(),
+            UnOp::BitNot => todo!(),
+            UnOp::AddressOf => todo!(),
+            UnOp::Deref => todo!(),
+        };
+        */
+        // check if value is out of bounds
+        // and replace value and  inner node if so
+        let concrete_inner = inner.node; //TODO:implement check
+        let concrete_value = value;
+
+        // create new unop with updated node
+        ConNode {
+            node : ExprNode {
+                data_type : data_type,
+                expr : Expr::UnOp(UnOpExpr::new(
+                        op,
+                        concrete_inner)),
+            },
+            value : concrete_value,
+        }
+
+   }
+
+/*
+    fn concretize_plus(&self, data_type, left, right) -> xxx {
+
+        // concretize if literals exceed bounds
+        let scalar_result = if matches!(left, Expr::Lit(Lit::I32(l)) &&
+                    matches!(right, Expr::Lit(Lit::I32(r)) {
+*/
+
+   
 }
