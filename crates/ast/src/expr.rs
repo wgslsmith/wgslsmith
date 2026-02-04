ae51c7ed7b857d70a533567ecd45102dc30b7929
mandryskowski <nedesf@gmail.com>
2026-02-04 20:31:18 +0000

    Fix literal writing

--- crates/ast/src/expr.rs
+++ crates/ast/src/expr.rs
@@ -27,22 +27,17 @@ impl Display for Lit {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Lit::Bool(v) => v.fmt(f),
-            Lit::I32(v) => v.fmt(f),
-            Lit::U32(v) => write!(f, "{v}u"),
-            Lit::F32(v) => {
-                write!(f, "{v}")?;
-
-                // The default rust formatting for f32 is to not print a decimal point if the
-                // number has no fractional component. This is problematic since WGSL will think
-                // it's an integer literal, so we manually add a decimal point in that case.
-                // TODO: Once naga supports the 'f' suffix for float literals we can switch to that
-                // https://github.com/gfx-rs/naga/pull/1863
-                if v.fract() == 0.0 {
-                    write!(f, ".0")?;
+            Lit::I32(v) => {
+                if *v == i32::MIN {
+                    write!(f, "i32({v})")?;
+                } else {
+                    write!(f, "{v}i")?;
                 }
 
                 Ok(())
             }
+            Lit::U32(v) => write!(f, "{v}u"),
+            Lit::F32(v) => write!(f, "{v}f"),
         }
     }
 }
